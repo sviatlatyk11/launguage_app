d9daf0909fd398399a6f2401c9fd9734fc5bd314
sviatlatyk11 <138723265+sviatlatyk11@users.noreply.github.com>
2023-07-25 11:18:35 +0100

    Add files via upload

---
+++ upload a json file to mongodb.py
@@ -0,0 +1,33 @@
+from pymongo import MongoClient
+
+# if I need to upload the file
+import json
+
+# connect to database
+connection = MongoClient("mongodb+srv://sviatoslavlatyk11:F3omowNLEcvi6uMn@connection0.urtg1xc.mongodb.net/?retryWrites=true&w=majority")
+
+
+# creating the database -> name 'Words'
+database = connection["Words"]
+
+# creating the collection -> name 'data'
+collection = database["data"]
+
+
+# uploading the file to mongodb
+
+# with open('data.json') as file:
+#     file_data = json.load(file)
+#
+# if isinstance(file_data, list):
+#     collection.insert_many(file_data)
+# else:
+#     collection.insert_one(file_data)
+
+
+# finding a word in the database
+for x in collection.find({"word": "animal"}):
+  print(x)
+
+
+
